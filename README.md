2a7bf9c201ca1e73e9c5cad018a00b29efe147cf
Jonathan Cone <jonathan.t.cone@gmail.com>
2012-08-13 19:01:53 -0500

    Added session support for socket.io.

---
+++ README.md
@@ -0,0 +1,11 @@
+The Hotdog Water Project 
+========================
+
+
+A real-time web chat application built on the following tech stack:
+* Node.js
+* Socket.io
+* Express.js
+* Twitter Bootstrap
+
+We're just getting started so don't get your undies in a bunch. 
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var nconf = require('nconf');
+var connect           = require('connect');
+var express           = require('express');
+var nconf             = require('nconf');
+var expressValidator  = require('express-validator');
+var io                = require('socket.io');
+var util              = require('util');
 
 nconf.argv().env().file({file: './config.json'});
-
 
 
 var OAuth = require('oauth').OAuth;
@@ -20,12 +23,17 @@ var oa = new OAuth(
 
 var app = express.createServer(express.logger(), express.bodyParser());
 
+var sessionStore = new express.session.MemoryStore();
+
+
 app.configure(function() {
   app.use(express.cookieParser());
-  app.use(express.session({ secret: nconf.get("server:session:secret") }));
+  app.use(express.session({ key: 'express.sid', secret: nconf.get("server:session:secret"), store: sessionStore }));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
   app.use(express.logger());
   app.use(express.static(__dirname + '/public'));
+  app.use(express.bodyParser());
+  app.use(expressValidator);
 
   app.set('view cache', false);
   app.set('view engine', 'ejs');
@@ -35,25 +43,50 @@ app.configure(function() {
 
 app.listen(8080);
 
-var io = require('socket.io');
-io = io.listen(app);
+var socket = io.listen(app);
+var parseCookie = connect.utils.parseCookie;
+ var Session = connect.middleware.session.Session;
 
-app.get('/oauth/twitter', function(req, res) {
-	oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
-		if (error) {
-			console.log(error);
-		}
-		else {
-			req.session.oauth = {};
-			req.session.oauth.token = oauth_token;
-			req.session.oauth.token_secret = oauth_token_secret;
-			res.redirect(nconf.get('twitter:url:authenticate') + oauth_token);
+socket.set('authorization', function (data, accept) {
+    if (data.headers.cookie) {
+        data.cookie = parseCookie(data.headers.cookie);
+        data.sessionID = data.cookie['express.sid'];
+        data.sessionStore = sessionStore;
+        sessionStore.get(data.sessionID, function (err, session) {
+            if (err || !session) {
+                accept('Error, could not get session.', false);
+            } else {
+                data.session = new Session(data, session);
+                accept(null, true);
+            }
+        });
+    } else {
+       return accept('No cookie transmitted.', false);
     }
-	});
+});
+
+
+socket.sockets.on('connection', function (socket) {
+    var hs = socket.handshake;
+    console.log('A socket with sessionID ' + hs.sessionID 
+        + ' connected!');
+
+    var intervalID = setInterval(function () {
+        hs.session.reload( function () { 
+            hs.session.touch().save();
+        });
+    }, 60 * 1000);
+    socket.on('disconnect', function () {
+        console.log('A socket with sessionID ' + hs.sessionID 
+            + ' disconnected!');
+        clearInterval(intervalID);
+    });
+ 
 });
 
 app.get('/oauth/callback', function(req, res, next) {
 	if (req.session.oauth) {
+
 		req.session.oauth.verifier = req.query.oauth_verifier;
 		var oauth = req.session.oauth;
 
@@ -77,9 +110,28 @@ app.get('/oauth/callback', function(req, res, next) {
   }
 });
 
-app.get('/chat', function(req, res) {
+app.post('/login', function(req, res) {
 
-  console.log(req.session);  
+  req.session.roomname = req.query.roomname;
+
+  oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
+ 
+    if (error) {
+      console.log("ERROR OCCURRED: " + error);
+    }
+    else {
+
+      req.session.oauth = {};
+      req.session.oauth.token = oauth_token;
+      req.session.oauth.token_secret = oauth_token_secret;
+      res.redirect(nconf.get('twitter:url:authenticate') + oauth_token);
+    }
+  });
+});
+
+
+
+app.get('/chat', function(req, res) {
 
   if(req.session.oauth) {
     res.render('chat', {req: req });
